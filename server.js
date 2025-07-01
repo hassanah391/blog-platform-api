6b506f440fbbade93d7cf60aca8dbe2a5fde52ba
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-07-01 09:25:03 +0300

    Mount router in server.js

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import { config } from './config.js';
+import routes from './routes/index.js'
 
 const port = config.serverPort;
 const app = express();
 
 app.use(express.json());
+app.use('/', routes); // All routing is handled here
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
 export default app;
-
