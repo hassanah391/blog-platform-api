4c4d82dc55c2f86c3837456f1ddac3dfb37b09b2
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-07-01 09:24:06 +0300

    Use 0.0.0.0 instead of localhost

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const dbPort = process.env.DB_PORT || 27017;
-const dbHost = process.env.DB_HOST || 'localhost';
+const dbHost = process.env.DB_HOST || '0.0.0.0';
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 const dbName = process.env.DB_NAME || 'blog_app';
 const serverPort = process.env.SERVER_PORT || 3000;
-const serverHost = process.env.SERVER_HOST || 'localhost';
+const serverHost = process.env.SERVER_HOST || '0.0.0.0';
 export const config = {
   dbPort,
   dbHost,
